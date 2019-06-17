1a719f91470e08c4f9cb79d61f89245adde6f0b3
kmgarvey279 <kmgarvey279@gmail.com>
2019-06-17 16:33:30 -0700

    Update Program.cs

--- QueenAttack/Models/Program.cs
+++ QueenAttack/Models/Program.cs
@@ -1,20 +1,17 @@
-
 using System;
-namespace  QueenSpace{
-
-
-
-public class Program
+namespace  QueenSpace
 {
-  public static void Main()
+  public class Program
   {
-    Console.WriteLine("Please enter X coordinate of your piece");
-    int userCoordinateX = int.Parse(Console.ReadLine());
-    Console.WriteLine("Please enter Y coordinate of your piece");
-    int userCoordinateY = int.Parse(Console.ReadLine());
-    Queen myCoord = new Queen ();
-    bool res = myCoord.CanAttack(userCoordinateX, userCoordinateY);
-    Console.WriteLine ("You can be attacked" + res);
+    public static void Main()
+    {
+      Console.WriteLine("Please enter X coordinate of your piece");
+      int userCoordinateX = int.Parse(Console.ReadLine());
+      Console.WriteLine("Please enter Y coordinate of your piece");
+      int userCoordinateY = int.Parse(Console.ReadLine());
+      Queen myCoord = new Queen ();
+      bool res = myCoord.CanAttack(userCoordinateX, userCoordinateY);
+      Console.WriteLine ("You can be attacked" + res);
+    }
   }
-}
 }
